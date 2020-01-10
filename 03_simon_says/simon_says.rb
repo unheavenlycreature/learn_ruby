1e0b0a3f42ee53a382021a974c24b8592b4782e5
unheavenlycreature <imaginaryenemy@protonmail.com>
2020-01-10 01:14:02 -0500

    Solutions for Simon Says.

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,36 @@
 #write your code here
+def echo(to_echo)
+  to_echo
+end
+
+def shout(to_shout)
+  to_shout.upcase
+end
+
+def repeat(to_repeat, repetitions = 2)
+  ((to_repeat + " ") * repetitions).strip
+end
+
+def start_of_word(word, prefix_length)
+  word[0..prefix_length - 1]
+end
+
+def first_word(words)
+  words.split[0]
+end
+
+def titleize(title)
+  little_words = ["and", "over", "the"]
+  titleized = []
+  words = title.split
+  titleized << words.first.capitalize
+  words.delete_at 0
+  words.each do |word|
+    if little_words.include? word
+      titleized << word
+    else
+      titleized << word.capitalize
+    end
+  end
+  titleized.join(" ")
+end
