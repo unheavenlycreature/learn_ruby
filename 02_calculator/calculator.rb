aeb69db7eb3a62cd8aefa335c687d375df3438e9
unheavenlycreature <imaginaryenemy@protonmail.com>
2020-01-10 00:55:43 -0500

    Add multiply, power, and factorial methods, in addition to tests for them.

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -11,3 +11,19 @@ end
 def sum(operands)
   operands.reduce(0) { |sum, n| add(sum, n) }
 end
+
+def multiply(operands)
+  operands.reduce(1) { |product, n| product = product * n }
+end
+
+def power(x, y)
+  x ** y
+end
+
+def factorial(x)
+  operands = []
+  x.times do |i|
+    operands << i + 1
+  end
+  multiply(operands)
+end
