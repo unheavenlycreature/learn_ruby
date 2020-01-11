f92a4caf8c2d36678c678699fa884ea27cffb445
unheavenlycreature <imaginaryenemy@protonmail.com>
2020-01-11 09:44:42 -0500

    Pig latin solution (yay regular expressions)

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,12 @@
-#write your code here
+def translate(sentence)
+  words = sentence.split
+  words.map! do |word|
+    prefix, vowel, suffix = word.partition(/[aeio]|(?<!q)u/)
+    if prefix == ""
+      vowel + suffix + "ay"
+    else
+      vowel + suffix + prefix + "ay"
+    end
+  end
+  words.join " "
+end
