3d6e1f06e85337e86602220e833d6e04b535e8b9
unheavenlycreature <imaginaryenemy@protonmail.com>
2020-01-11 10:30:49 -0500

    Timer solutions.

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,30 @@
 class Timer
-  #write your code here
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds
+    @seconds
+  end
+
+  def seconds=(seconds)
+    @seconds = seconds
+  end
+
+  def time_string
+    seconds = @seconds
+    hours = seconds / 3600
+    seconds = seconds - (hours * 3600)
+    minutes = seconds / 60
+    seconds = seconds - (minutes * 60)
+    [padded(hours), padded(minutes), padded(seconds)].join(":")
+  end
+
+  def padded(num)
+    if num < 10
+      "0" + num.to_s
+    else
+      num.to_s
+    end
+  end
 end
